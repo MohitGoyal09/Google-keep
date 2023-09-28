685dff9e4689617bbfd15d909cd5430a36028ec3
MohitGoyal09 <goel0632@gmail.com>
2023-09-28 17:57:30 +0530

    fixed delete btn

--- script.js
+++ script.js
@@ -1,57 +1,72 @@
-const addtitle = document.getElementById('addTitle');
-const addtext = document.getElementById('addText');
-const addbtn = document.getElementById('addNote');
-const notes = document.getElementById('notes');
+const addtitle = document.getElementById("addTitle");
+const addtext = document.getElementById("addText");
+const addbtn = document.getElementById("addNote");
+const notes = document.getElementById("notes");
 shownotes();
 
 function addnotes() {
-    let notesobj = localStorage.getItem('notes');
-    if (notesobj == null) {
-        notesobj = [];
-    }
-    else{
-        notesobj = JSON.parse(notesobj);
-    }
-    if (addtitle.value.length !== 0 && addtext.value.length !== 0) {
-        let note = {
-            title: addtitle.value,
-            text: addtext.value
-        };
-        notesobj.push(note);
-        addtitle.value = '';
-        addtext.value = '';
-        console.log(notesobj);
-        localStorage.setItem('notes', JSON.stringify(notesobj));
-        shownotes();
-    }
+  let notesobj = localStorage.getItem("notes");
+  if (notesobj == null) {
+    notesobj = [];
+  } else {
+    notesobj = JSON.parse(notesobj);
+  }
+  if (addtitle.value.length !== 0 && addtext.value.length !== 0) {
+    let note = {
+      title: addtitle.value,
+      text: addtext.value,
+    };
+    notesobj.push(note);
+    addtitle.value = "";
+    addtext.value = "";
+    console.log(notesobj);
+    localStorage.setItem("notes", JSON.stringify(notesobj));
+    shownotes();
+  }
 }
 
-addbtn.addEventListener('click', addnotes);
+addbtn.addEventListener("click", addnotes);
 
-function shownotes(){
-    notes.innerHTML = '';
-    let notesobj = localStorage.getItem("notes");
-    if (notesobj == null) {
-      notesobj = [];
-    } else {
-      notesobj = JSON.parse(notesobj);
-    }
-    notesobj.forEach(function(element){
-        let note = document.createElement('div');
-        note.classList.add('note');
-        let title = document.createElement('h2');
-        let text = document.createElement('p');
-        let deletebtn = document.createElement('button');
-        deletebtn.innerHTML = 'Delete';
+function shownotes() {
+  notes.innerHTML = "";
+  let notesobj = localStorage.getItem("notes");
+  if (notesobj == null) {
+    notesobj = [];
+  } else {
+    notesobj = JSON.parse(notesobj);
+  }
+  notesobj.forEach(function (element) {
+    let note = document.createElement("div");
+    note.classList.add("note");
+    let title = document.createElement("h2");
+    let text = document.createElement("p");
+    let deletebtn = document.createElement("button");
+    deletebtn.innerHTML = "Delete";
+    let editbtn = document.createElement("button");
+    editbtn.innerHTML = "Edit";
 
-        deletebtn.addEventListener('click', function(){
-            deletebtn.parentElement.remove();
-        })
-        title.innerText = element.title;
-        text.innerText = element.text;
-        note.appendChild(title);
-        note.appendChild(text);
-        note.appendChild(deletebtn);
-        notes.appendChild(note);
+    deletebtn.addEventListener("click", function () {
+      deletenotes(element.title);
     });
+    title.innerText = element.title;
+    text.innerText = element.text;
+    note.appendChild(title);
+    note.appendChild(text);
+    note.appendChild(deletebtn);
+    notes.appendChild(note);
+  });
+}
+
+function deletenotes(title) {
+  let notesobj = localStorage.getItem("notes");
+  if (notesobj == null) {
+    notesobj = [];
+  } else {
+    notesobj = JSON.parse(notesobj);
+  }
+  notesobj = notesobj.filter(function (note) {
+    return note.title !== title;
+  });
+  localStorage.setItem("notes", JSON.stringify(notesobj));
+  shownotes();
 }
